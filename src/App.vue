3b8fe16d659e2f5fdf4a6ce5de45282daef958c4
Olya Severova <34347298+olyaseverova@users.noreply.github.com>
2021-02-15 00:00:25 +0300

    RADIO main page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app" class="home">
+    <h1 class="main-h1 up">Дубрава-радио онлайн</h1>
+    <h3 class="main-h3">Приветствуем Вас на портале «Дубрава-радио онлайн».</h3>
+    <p class="main-text">
+      Ежедневно и круглосуточно здесь звучит музыка, которую мы создаём и
+      исполняем для Вас, ценителей живого и настоящего искусства. Русская
+      современная авторская музыка, обработки шедевров русской и мировой
+      классики, современный фольклор и классический джаз.
+    </p>
+    <img class="home__img" src="@/img/main.jpg" alt="Логотип" />
+    <ul class="audios-list list">
+      <li class="audios-list__item" v-for="audio in audios" :key="audio.id">
+        <p class="audios-list__title">{{ audio.title }}</p>
+        <audio controls :src="audio.src"></audio>
+      </li>
+    </ul>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  data() {
+    return {
+      audios: [
+        {
+          src: "http://89.22.156.37:8000/mpd.128",
+          title: "Arimoia Trio - ГђВ§ГђВµГђВ»ГђВѕГђВІГђВµГђВє",
+        },
+        {
+          src: "http://89.22.156.37:8000/mpd.320",
+          title: "Arimoia Trio - Ð§ÐµÐ»Ð¾Ð²ÐµÐº",
+        },
+        {
+          src: "http://89.22.156.37:8000/mpd.64",
+          title: "Arimoia Trio - ├Р┬з├Р┬╡├Р┬╗├Р┬╛├Р┬▓├Р┬╡├Р┬║",
+        },
+        {
+          src: "http://89.22.156.37:8000/mpd.ogg",
+          title: "Arimoia Trio - Человек",
+        },
+      ],
+    };
+  },
+};
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
